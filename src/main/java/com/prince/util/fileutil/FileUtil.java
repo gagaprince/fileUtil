009fbb6df84dd0f5cb7f06cc14186fc6935c093a
zidong.wang <zidong.wang@qunar.com>
2015-12-15 12:48:30 +0800

    fileutil init

---
+++ src/main/java/com/prince/util/fileutil/FileUtil.java
@@ -0,0 +1,64 @@
+package com.prince.util.fileutil;
+
+import java.io.*;
+
+/**
+ * Created by zidong.wang on 2015/12/15.
+ */
+public class FileUtil {
+    private static FileUtil fileUtil;
+    public static FileUtil getInstance(){
+        if (fileUtil==null){
+            fileUtil = new FileUtil();
+        }
+        return fileUtil;
+    }
+
+    public File createFile(File pathFile){
+        if(!pathFile.exists()){
+            try {
+                String parentPath = pathFile.getParent();
+                File parentFile = new File(parentPath);
+                if(!parentFile.isDirectory()){
+                    parentFile.mkdirs();
+                }
+                pathFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return pathFile;
+    }
+
+    public File createFile(String path){
+        File pathFile = new File(path);
+        return createFile(pathFile);
+    }
+
+    public void deleteFile(File pathFile){
+        if(pathFile.exists()){
+            pathFile.delete();
+        }
+    }
+
+    public void deleteFile(String path){
+        File pathFile = new File(path);
+        deleteFile(pathFile);
+    }
+
+    public void saveInputStreamInFile(InputStream in ,String path){
+        File des = createFile(path);
+        try {
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(des));
+            BufferedInputStream bis = new BufferedInputStream(in);
+            byte[] bytes = new byte[1024];
+            int len = 0;
+            while ((len = bis.read(bytes))!=-1){
+                bos.write(bytes,0,len);
+            }
+            bos.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/prince/util/AppTest.java
@@ -0,0 +1,42 @@
+package com.prince.util;
+
+import com.prince.util.fileutil.FileUtil;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testCreateFile()
+    {
+        FileUtil fileUtil = FileUtil.getInstance();
+        String filePath = "d:/aa/bb/cc/aa.txt";
+//        fileUtil.createFile(filePath);
+//        fileUtil.deleteFile(filePath);
+    }
+}
