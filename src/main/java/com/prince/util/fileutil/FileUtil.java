8efcd1737425a897f5cf5b1448fe5a0a7244ec04
zidong.wang <zidong.wang@qunar.com>
2016-01-11 13:32:38 +0800

    add some fun

--- src/main/java/com/prince/util/fileutil/FileUtil.java
+++ src/main/java/com/prince/util/fileutil/FileUtil.java
@@ -2,6 +2,7 @@ package com.prince.util.fileutil;
 
 import java.io.*;
 import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  * Created by zidong.wang on 2015/12/15.
@@ -74,5 +75,85 @@ public class FileUtil {
         }catch (IOException e){
             e.printStackTrace();
         }
+    }
+
+    public String getContentFromFile(String path){
+        File f = new File(path);
+        return getContentFromFile(f);
+    }
+    public String getContentFromFile(File file){
+        StringBuffer sb = new StringBuffer("");
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(file),"utf-8"));
+            String line = null;
+            while ((line=br.readLine())!=null){
+                sb.append(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(br!=null){
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    public void saveContentInFile(String path,String content){
+        File f = new File(path);
+        saveContentInFile(f,content);
+    }
+
+    public void saveContentInFile(File f,String content){
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(f),"utf-8"));
+            bw.write(content);
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(bw!=null){
+                try {
+                    bw.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public String unicodeToGB(String s)   {
+        StringBuffer sb = new StringBuffer();
+        StringTokenizer st = new StringTokenizer(s,"\\\\u");
+        while (st.hasMoreTokens())   {
+            sb.append((char)Integer.parseInt(st.nextToken(),16));
+        }
+        return sb.toString();
+    }
+
+    public String unicode2String(String unicode) {
+
+        StringBuffer string = new StringBuffer();
+
+        String[] hex = unicode.split("\\\\u");
+        for (int i = 1; i < hex.length; i++) {
+
+            // 转换出每一个代码点
+            int data = Integer.parseInt(hex[i], 16);
+            // 追加成string
+            string.append((char) data);
+        }
+
+        return string.toString();
     }
 }
